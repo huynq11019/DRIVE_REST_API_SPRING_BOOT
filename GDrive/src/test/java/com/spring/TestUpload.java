5190a67fe1ddf38b4188ec006cf35a9af25ac309
Trương Tam Lang <lang.tt16@gmail.com>
2018-01-07 17:26:51 +0700

    first commit
    first commit

---
+++ GDrive/src/test/java/com/spring/TestUpload.java
@@ -0,0 +1,32 @@
+package com.spring;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.spring.service.GoogleDriveService;
+
+@RunWith(SpringRunner.class)
+@SpringBootApplication
+public class TestUpload {
+		@Autowired
+		GoogleDriveService driveService;
+	
+	@Test
+	public void testUpload() {
+		File file = new File("C:\\Users\\ttlang\\Downloads\\pic\\hinh.png");
+		com.google.api.services.drive.model.File file2  = driveService.upLoadFile(file.getName(), file.getAbsolutePath(),"image/jpg");
+		try {
+			System.err.println(file2.toPrettyString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
